43c73ccfde267619a67c9d09673da4f93b725c5e
Tim Park <timfpark@gmail.com>
2013-03-13 14:48:09 -0700

    bump version

--- magenta/agent.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-
-function Agent(json) {
-    for(var key in json) {
-        if(json.hasOwnProperty(key)) {
-            this[key] = json[key];
-        }
-    }
-}
