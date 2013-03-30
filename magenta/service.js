7753ff3eb8da6abf9eb348df764bc7f2ec34d253
Tim Park <timfpark@gmail.com>
2013-03-29 19:01:03 -0700

    add headwaiter configuration of service

--- magenta/service.js
+++ magenta/service.js
@@ -1,4 +1,5 @@
-var Session = require('./session');
+var request = require('request')
+  , Session = require('./session');
 
 function Service(config) {
 	this.config = config;
@@ -9,7 +10,7 @@ Service.prototype.connect = function(principal, callback) {
     var self=this;
 
     self.store.load(function(err) {
-        if (err) callback(err, null);
+        if (err) return callback(err, null);
 
         self.register(principal, function(err, principal) {
             if (err) return callback(err, null, null);
@@ -21,30 +22,52 @@ Service.prototype.connect = function(principal, callback) {
     });
 };
 
+Service.prototype.configure = function(config, principal, callback) {
+    var headwaiter_url = config.headwaiter_endpoint;
+    if (principal) {
+        headwaiter_url += "?principal_id=" + principal.id;
+    }
+
+    request.get({url: headwaiter_url, json: true}, function(err, resp, body) {
+        if (err) return callback(err, null);
+        if (resp.statusCode != 200) return callback("messages saveMany http response: " + resp.statusCode, null);
+
+        for (var key in body.endpoints) {
+            config[key] = body.endpoints[key];
+        }
+
+        callback(null, config);
+    });
+};
+
 Service.prototype.register = function(principal, callback) {
     var storedPrincipal = this.store.get(principal.toStoreId());
     var self=this;
 
-    // hit the directory endpoint to get endpoints for principal
+    this.configure(self.config, storedPrincipal, function(err, config) {
+        if (err) return callback(err);
 
-    if (!storedPrincipal) {
-        console.log("need to provision principal");
-        principal.create(self.config, function(err, principal) {
-            if (err) {
-                console.log('failed to provision principal: ' + err);
-                callback(err, null);
-            }
+        self.config = config;
 
-            console.log("principal provisioned: " + JSON.stringify(principal));
+        if (!storedPrincipal) {
+            console.log("need to provision principal");
+            principal.create(self.config, function(err, principal) {
+                if (err) {
+                    console.log('failed to provision principal: ' + err);
+                    callback(err, null);
+                }
 
-            self.store.set(principal.toStoreId(), principal);
+                console.log("principal provisioned: " + JSON.stringify(principal));
+
+                self.store.set(principal.toStoreId(), principal);
+                callback(null, principal);
+            });
+        } else {
+            console.log("TODO: need to relogin principal");
+            principal.id = storedPrincipal.id;
             callback(null, principal);
-        });
-    } else {
-        console.log("TODO: need to relogin principal");
-        principal.id = storedPrincipal.id;
-        callback(null, principal);
-    }
+        }
+    });
 
 };
 
