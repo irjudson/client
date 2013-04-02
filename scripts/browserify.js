9fd269092c0595a187a7fd00ffe990828e66d7cc
Tim Park <timfpark@gmail.com>
2013-04-02 11:19:03 -0700

    substitute browser-request for request when browserifying

--- scripts/browserify.js
+++ scripts/browserify.js
@@ -1,2 +1,8 @@
+var browserify = require('browserify');
+var b = browserify();
+b.require('../node_modules/browser-request/dist/browser/request.js', {expose: 'request'});
+
+var request = require('request');
+
 window.Buffer = require('buffer').Buffer;
 window.magenta = require('../magenta');
