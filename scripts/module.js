bee0197f3c1b6fd7a668d9a4f8d26a6b4f283763
Tim Park <timfpark@gmail.com>
2013-04-02 19:48:04 -0700

    build browser client with custom script

---
+++ scripts/module.js
@@ -0,0 +1,11 @@
+window.magenta = {
+  Blob: Blob,
+  Device: Device,
+  EventLog: EventLog,
+  HTML5Store: HTML5Store,
+  Message: Message,
+  Principal: Principal,
+  Service: Service,
+  Session: Session,
+  User: User
+};
