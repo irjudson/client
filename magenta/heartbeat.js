0003200aed45160a5b338a90f9e44b024cd10140
Tim Park <timfpark@gmail.com>
2013-04-10 19:59:47 -0700

    fix heartbeat this reference

--- magenta/heartbeat.js
+++ magenta/heartbeat.js
@@ -9,15 +9,12 @@ Heartbeat.prototype.start = function() {
     if (this.heartbeatInterval) return;
     var self = this;
 
-    console.log("starting heartbeat");
-
     this.heartbeatInterval = setInterval(function() {
         var message = new Message();
         message.message_type = "heartbeat";
 
-        console.log("sending heartbeat");
-        message.save(this.session, function(err, message) {
-            if (err) { return this.session.log.error("failed to send heartbeat"); }
+        message.save(self.session, function(err, message) {
+            if (err) { return self.session.log.error("failed to send heartbeat"); }
         });
     }, self.session.service.config.heartbeat_period || 10 * 60 * 1000);
 };
