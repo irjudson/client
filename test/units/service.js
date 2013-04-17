1f7e6cb2b96b8099d547b0c0660160ef62b8f943
Tim Park <timfpark@gmail.com>
2013-04-17 06:59:23 -0700

    add impersonation to client library

--- test/units/service.js
+++ test/units/service.js
@@ -1,10 +1,12 @@
-var assert = require('assert'),
-	config = require('../config'),
-	nitrogen = require('../../lib');
+var assert = require('assert')
+  , config = require('../config')
+  , fixtures = require('../fixtures')
+  ,	nitrogen = require('../../lib');
 
 describe('service object', function() {
 
-	var camera = new nitrogen.Device({ capabilities: "camera", local_id: "camera" });
+    var camera = new nitrogen.Device({ capabilities: ["camera"], local_id: "camera" });
+    var thermometer = new nitrogen.Device({ capabilities: ["thermometer"], local_id: "thermometer" });
 
 	it('should be able to connect device', function(done) {
         var service = new nitrogen.Service(config);
@@ -17,5 +19,34 @@ describe('service object', function() {
             done();
         });
 	});
+
+    it('camera should be able to impersonate itself', function(done) {
+        var service = new nitrogen.Service(config);
+        service.connect(fixtures.models.camera, function(err, session, cameraPrincipal) {
+            assert.equal(err, null);
+            assert.notEqual(session, null);
+
+            service.impersonate(session, cameraPrincipal, function(err, impersonationSession) {
+                assert.equal(err, null);
+                assert.notEqual(impersonationSession, null);
+
+                done();
+            });
+        });
+    });
+
+    it('thermometer should be not be able to impersonate the camera', function(done) {
+        var service = new nitrogen.Service(config);
+        service.connect(thermometer, function(err, session, thermometer) {
+            assert.equal(err, null);
+            assert.notEqual(session, null);
+
+            service.impersonate(session, fixtures.models.camera, function(err, session) {
+                assert.equal(err, 401);
+
+                done();
+            });
+        });
+    });
 
 });
