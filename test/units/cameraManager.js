6c12686a6886be8c7c5a926fcd095d9c0c090ef4
Tim Park <timfpark@gmail.com>
2013-08-02 12:54:59 -0700

    remove dependency on opencv and move out to seperate module

--- test/units/cameraManager.js
+++ test/units/cameraManager.js
@@ -3,51 +3,6 @@ var assert = require('assert')
   , path = require('path');
 
 describe('cameraManager', function() {
-    it('should detect motion', function(done) {
-        var cameraManager = new nitrogen.CameraManager();
-
-        var command = new nitrogen.Message({
-            id: '1',
-            type: 'cameraCommand',
-            expires: new Date(2050,1,1,0,0,0),
-            body: {
-                command: 'motion'
-            }
-        });
-
-        cameraManager.history = [
-            { path: path.join(__dirname, '../fixtures/images/motion0.jpg') },
-            { path: path.join(__dirname, '../fixtures/images/motion2.jpg') }
-        ];
-
-        cameraManager.detectMotion(command, function(motion) {
-            assert.equal(motion, true);
-            done();
-        });
-    });
-
-    it('should not detect motion with static images', function(done) {
-        var cameraManager = new nitrogen.CameraManager();
-
-        var command = new nitrogen.Message({
-            id: '1',
-            type: 'cameraCommand',
-            expires: new Date(2050,1,1,0,0,0),
-            body: {
-                command: 'motion'
-            }
-        });
-
-        cameraManager.history = [
-            { path: path.join(__dirname, '../fixtures/images/motion0.jpg') },
-            { path: path.join(__dirname, '../fixtures/images/motion0.jpg') }
-        ];
-
-        cameraManager.detectMotion(command, function(motion) {
-            assert.equal(motion, false);
-            done();
-        });
-    });
 
     it('should be able to calculate the right amount of history required', function(done) {
         var cameraManager = new nitrogen.CameraManager();
