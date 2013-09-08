80726cbaa223e1a5fa9351ee2c2bd7ffc441dda0
Tim Park <timfpark@gmail.com>
2013-09-08 11:53:59 +0100

    documentation fixes

---
+++ docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "nitrogen.Agent",
+        "nitrogen.CommandManager",
+        "nitrogen.Device",
+        "nitrogen.Message",
+        "nitrogen.Principal",
+        "nitrogen.Service",
+        "nitrogen.Session",
+        "nitrogen.User"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
