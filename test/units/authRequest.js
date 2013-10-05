8ebfd30cdf998eaca2606c25a54cd1524507c472
Tim Park <timfpark@gmail.com>
2013-10-05 19:14:38 +0200

    update access token on request from service

---
+++ test/units/authRequest.js
@@ -0,0 +1,45 @@
+var assert = require('assert'),
+    AuthRequest = require('../../lib/authRequest');
+
+describe('authRequest', function() {
+
+    it('receiving a set-access-token header should update principal and session access token', function(done) {
+        var resp = {
+            headers: {
+                "X-n2-set-access-token": JSON.stringify({ 
+                    token: 'rwvrVSVPKRMwdd115FSW8kRbgxcv1mEkhwgFQ2Aqn7iRbaqPNqAEMNbyomd6fWKYVbXeHEH9mEDJqlxYP+QlQw==',
+                    expires_at: new Date(2050, 1, 1),
+                    created_at: new Date(),
+                    id: '5250453b83dce2433d000008'
+                })
+            }
+        };
+
+        var session = {
+            accessToken: {
+                token: 'notupdated'
+            },
+            principal: {
+                accessToken: {
+                    token: 'notupdated'
+                },
+                toStoreId: function() {},
+                toStoreObject: function() {},
+
+            },
+            service: {
+                store: {
+                    set: function() {}
+                }
+            }
+        };
+
+        AuthRequest.afterRequest(session, null, resp, null, function(err, resp, body) {
+            assert.notEqual(session.accessToken.token, 'notupdated');
+            assert.notEqual(session.principal.accessToken.token, 'notupdated');
+
+            done();
+        });
+    });
+ 
+});
