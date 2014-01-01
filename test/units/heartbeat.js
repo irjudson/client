d218065d43a2b4ca9fa7a5341f8e54d2b192e9d0
Tim Park <timfpark@gmail.com>
2013-12-31 19:47:07 -0800

    fix typo in heartbeat and add test to cover

---
+++ test/units/heartbeat.js
@@ -0,0 +1,24 @@
+var assert = require('assert')
+  , config = require('../config')
+  , nitrogen = require('../../lib');
+
+describe('heartbeat', function() {
+
+    it('should be able to send a heartbeat', function(done) {
+        var service = new nitrogen.Service(config);
+
+        var device = new nitrogen.Device({
+            capabilities: "camera",
+            nickname: "camera"
+        });
+
+        service.create(device, function(err, session, principal) {
+            session.heartbeat.send(function(err) {
+                assert.ifError(err);
+
+                done();
+            });
+        });
+    });
+
+});
