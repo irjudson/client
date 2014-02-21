f07f549abf40b0cb8c10129dd0360ba007ef360f
Tim Park <timfpark@gmail.com>
2014-02-20 19:44:25 -0800

    add ability to delete permission from client lib

--- test/units/permissions.js
+++ test/units/permissions.js
@@ -9,21 +9,38 @@ describe('permission', function() {
         nickname: "camera"
     });
 
-    it('create should work', function(done) {
+    it('should be able to create, find, and remove permissions', function(done) {
         var service = new nitrogen.Service(config);
         service.connect(camera, function(err, session) {
             var permission = new nitrogen.Permission({
                 issued_to:     camera.id,
                 principal_for: camera.id,
-                action:       'send',
-                priority:     100000000,
-                authorized:   true        
+                priority:      100000000,
+                authorized:    true        
             });
 
             permission.create(session, function(err, permission) {
                 assert.ifError(err);
                 assert.notEqual(permission.id, undefined);
-                done();
+
+                nitrogen.Permission.find(session, { issued_to: camera.id }, {}, function(err, permissions) {
+                    assert.ifError(err);
+                    var startingLength = permissions.length;
+
+                    assert.equal(startingLength, 2);
+
+                    permission.remove(session, function(err) {
+                        assert.ifError(err);
+
+                        nitrogen.Permission.find(session, { issued_to: camera.id }, {}, function(err, newPermissions) {
+                            assert.ifError(err);
+                            var endingLength = newPermissions.length;
+
+                            assert.equal(endingLength, startingLength - 1);
+                            done();
+                        });
+                    });
+                });
             });
         });
     });
