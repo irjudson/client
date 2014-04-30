36226bdcfb51b5b8980d1224723e5a71c852ee2b
Tim Park <timfpark@gmail.com>
2014-04-30 06:48:26 -0700

    use test service for testing

--- test/config.js
+++ test/config.js
@@ -4,7 +4,7 @@ var nitrogen = require('../lib'),
 var config = { 
     host: "localhost",
     protocol: "http",
-    http_port: 3030
+    http_port: 3050
 };
 
 config.store = new MemoryStore(config);
